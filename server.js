8055a39142612c3c9f4ff9cfa42aff77b22b5723
Yash Agrawal <yagrawl2@gmail.com>
2018-02-28 22:17:42 -0600

    update cert 4

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.get('/project/:name', (req, res) => {
     res.render('project/' + name, {});
 });
 
-app.get('/.well-known/acme-challenge/iVxHfJuCdXleLY3q7RqEdrEPzz7PLJspwZkIxJQSBtA', (req, res)=>{
- res.send('iVxHfJuCdXleLY3q7RqEdrEPzz7PLJspwZkIxJQSBtA.MaGiXMpQ1GUptMbPN3Keep2W4Lg0sOY4QbxekyYrnP0');
+app.get('/.well-known/acme-challenge/ap1KqFTOUppxAIccStja6aRWyocLknWKugrjL68r8No', (req, res)=>{
+ res.send('ap1KqFTOUppxAIccStja6aRWyocLknWKugrjL68r8No.MaGiXMpQ1GUptMbPN3Keep2W4Lg0sOY4QbxekyYrnP0');
 })
 
 app.get('*', (req, res) => {
