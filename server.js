da1c1e4b1bdc187171af8ab30d1d9704b17285e1
Yash Agrawal <yagrawl2@gmail.com>
2018-02-28 22:44:43 -0600

    maybe working?

--- server.js
+++ server.js
@@ -29,10 +29,6 @@ app.get('/project/:name', (req, res) => {
     res.render('project/' + name, {});
 });
 
-app.get('/.well-known/acme-challenge/DoGbfO2zOows_Y2ZyCnAdWJg5efnNwI_0RnBrKF2xCg', (req, res)=>{
- res.send('DoGbfO2zOows_Y2ZyCnAdWJg5efnNwI_0RnBrKF2xCg.MaGiXMpQ1GUptMbPN3Keep2W4Lg0sOY4QbxekyYrnP0');
-})
-
 app.get('*', (req, res) => {
     res.render('404', {});
 });
